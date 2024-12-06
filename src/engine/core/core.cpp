50c2643b25e195c408d1bb1b88d79dda240ebba1
abdullahgulcur <34477891+abdullahgulcur@users.noreply.github.com>
2024-12-06 16:10:27 +0300

    Add initial files

---
+++ src/engine/core/core.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "core.h"
+
+namespace Engine {
+
+    Core::Core() {
+
+    }
+
+    Core::~Core() {
+
+    }
+
+    void Core::initialize() {
+
+    }
+
+    void Core::run() {
+
+    }
+
+
+}
---
+++ src/engine/core/core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Engine {
+
+    class Core {
+    private:
+    public:
+        Core();
+        ~Core();
+        void initialize();
+        void run();
+    };
+}
