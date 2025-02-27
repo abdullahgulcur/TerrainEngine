d4d75a1e9376f97d7540943e1570e17d3ef05e27
abdullahgulcur <34477891+abdullahgulcur@users.noreply.github.com>
2025-02-27 17:13:39 +0300

    path bugfix

--- src/engine/image/image_gen.cpp
+++ src/engine/image/image_gen.cpp
@@ -1,12 +1,15 @@
 #include "pch.h"
 #include "image_gen.h"
 #include "gl_context.h"
+#include "config.h"
 
 namespace Engine {
 
 	unsigned int ImageGenerator::generatePerlinNoiseTexture(glm::u16vec2 size, float scale) {
 
-		unsigned int shaderProgramId = GLShader::loadShader("../../../resource/shader/generative/common/pnoise.vert", "../../../resource/shader/generative/common/pnoise.frag");
+		std::string resource_dir(RESOURCES);
+
+		unsigned int shaderProgramId = GLShader::loadShader(resource_dir + "/shader/generative/common/pnoise.vert", resource_dir + "/shader/generative/common/pnoise.frag");
 		unsigned int textureId = GLTexture::generateMaskTexture(size.x, size.y);
 		unsigned int FBO = GLBuffer::createFBO(textureId);
 		unsigned int planeVAO = GLBuffer::createQuadVAO();
